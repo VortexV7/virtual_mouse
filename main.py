e8f1ad38037b95d686f65ec22527c41e1590188c
Billy Butcher <78638686+VortexV7@users.noreply.github.com>
2024-04-16 23:05:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
-index_y = 0
+index_y = 1
 
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR_RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
     if hands:
